1da3a310656f89afa3ed1f9b9fb32e32ed35f7aa
Lucca Mendes Tomazetto <luccatomazetto@gmail.com>
2023-11-23 00:05:46 -0300

    Feat: m3 setup and viewmodel
    modify viewmodel so it can request the strings from the Resources of the application
    
    Also initial setup of the material design 3
    
    changes verified on physical device

--- app/src/main/java/com/example/dailystuff/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/dailystuff/ui/home/HomeViewModel.kt
@@ -1,13 +1,18 @@
 package com.example.dailystuff.ui.home
 
+import android.app.Application
+import android.content.Context
+import android.content.res.Resources
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.dailystuff.R
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = application.getString(R.string.hello_user)
     }
     val text: LiveData<String> = _text
 }
