9a4df452b11166ca99dd7d1f487944724d1a6f84
Lucca Mendes Tomazetto <luccatomazetto@gmail.com>
2023-12-04 20:35:04 -0300

    refactor: MVVM standards complete
    Fully functional MVVM standards inside the whole application

--- app/src/main/java/com/example/dailystuff/ui/apod/ApodFragment.kt
+++ app/src/main/java/com/example/dailystuff/ui/apod/ApodFragment.kt
@@ -8,7 +8,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.dailystuff.R
 import com.example.dailystuff.data.api.apod.APODApiClient
 import com.example.dailystuff.databinding.FragmentApodBinding
@@ -37,13 +40,48 @@ class ApodFragment : Fragment() {
         binding.vm = apodViewModel
         binding.lifecycleOwner = this
 
+        observe(apodViewModel)
+
+
         apodViewModel.fetchAPOD()
 
         return binding.root
+    }
+
+    private fun observe(apodViewModel: ApodViewModel) {
+
+        //Insert the image via Picasso
+        apodViewModel.apodImageUrl.observe(viewLifecycleOwner) {
+            apodViewModel.loadAPODImage(binding.apodImagePictureOfTheDay, it)
+        }
+
+        //Change the loading state on the application
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                apodViewModel.isLoading.collect {
+                    if (it) {
+                        binding.apodImagePictureOfTheDay.visibility = View.GONE
+                        binding.apodTextApodTitle.visibility = View.GONE
+                        binding.apodTextPictureDescription.visibility = View.GONE
+                        binding.apodTextFragmentDescription.visibility = View.GONE
+                        binding.apodLoadingState.visibility = View.VISIBLE
+                    } else {
+                        binding.apodImagePictureOfTheDay.visibility = View.VISIBLE
+                        binding.apodTextApodTitle.visibility = View.VISIBLE
+                        binding.apodTextFragmentDescription.visibility = View.VISIBLE
+                        binding.apodTextPictureDescription.visibility = View.VISIBLE
+                        binding.apodLoadingState.visibility = View.GONE
+
+                    }
+
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/example/dailystuff/ui/apod/ApodViewModel.kt
+++ app/src/main/java/com/example/dailystuff/ui/apod/ApodViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.dailystuff.ui.apod
 
+import android.widget.ImageView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.dailystuff.data.repository.ApodRepository
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,8 +16,10 @@ class ApodViewModel : ViewModel() {
 
     private val apodRepository = ApodRepository()
 
-    private var _apodImage = MutableLiveData<String>()
-    val apodImage: LiveData<String> = _apodImage
+    //Variables that are shown in the screen
+
+    private var _apodImageUrl = MutableLiveData<String>()
+    val apodImageUrl: LiveData<String> = _apodImageUrl
 
     private var _apodTitle = MutableLiveData("Image Title")
     val apodTitle: LiveData<String> = _apodTitle
@@ -23,6 +27,13 @@ class ApodViewModel : ViewModel() {
     private var _apodDescription = MutableLiveData("Image Description")
     val apodDescription: LiveData<String> = _apodDescription
 
+    //Loading State
+
+    private var _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+
+    // Possible Errors
     private var _hasErrorOcurred = MutableStateFlow(false)
     val hasErrorOcurred: StateFlow<Boolean> = _hasErrorOcurred
 
@@ -30,10 +41,11 @@ class ApodViewModel : ViewModel() {
     val errorMessage: LiveData<String> = _errorMessage
 
     fun fetchAPOD(){
+        _isLoading.value = true
         CoroutineScope(Dispatchers.IO).launch {
             val response = apodRepository.getAPOD()
             if (response.success){
-                _apodImage.postValue(response.data?.imageUrl)
+                _apodImageUrl.postValue(response.data?.imageUrl)
                 _apodTitle.postValue(response.data?.title)
                 _apodDescription.postValue(response.data?.description)
                 _hasErrorOcurred.value = false
@@ -41,6 +53,13 @@ class ApodViewModel : ViewModel() {
                 _hasErrorOcurred.value = true
                 _errorMessage.postValue(response.message)
             }
+            _isLoading.value = false
+        }
+    }
+
+    fun loadAPODImage(imageView: ImageView, imageUrl: String){
+        imageUrl.let{
+            Picasso.get().load(it).into(imageView)
         }
     }
 }
